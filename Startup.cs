a32f7bea6b64183bbfbf90e476dcf5ec10afbff8
ryandefea <ryan.defea@gmail.com>
2022-08-17 12:47:42 -0500

    add swagger info to startup
    Co-authored-by: Sandra Tena <SandraT22@users.noreply.github.com>
    Co-authored-by: vangpeter06 <vangpeter06@users.noreply.github.com>

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using TravelTime.Models;
-
+using Microsoft.OpenApi.Models;
 namespace TravelTime
 {
   public class Startup
@@ -16,15 +16,33 @@ namespace TravelTime
     }
 
     public IConfiguration Configuration { get; }
-    
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-        services.AddDbContext<TravelTimeContext>(opt =>
-        opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
-        services.AddControllers();
+
+      services.AddDbContext<TravelTimeContext>(opt =>
+          opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
+      services.AddControllers();
+
+      services.AddMvc();
+
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v2", new OpenApiInfo
+        {
+          Title = "Travel Time API",
+          Version = "v2",
+          Description = "Travel Information API",
+          Contact = new OpenApiContact
+          {
+            Name = "Ryan",
+            Email = "ryan.defea@gmail.com"
+          }
+        });
+      });
     }
+
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
@@ -32,10 +50,18 @@ namespace TravelTime
       {
         app.UseDeveloperExceptionPage();
       }
+      // app.UseSwagger
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+      });
 
       // app.UseHttpsRedirection();
 
       app.UseRouting();
+
+      // app.UseAuthentication();
 
       app.UseAuthorization();
 
